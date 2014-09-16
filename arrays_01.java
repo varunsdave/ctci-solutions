22e31127a540740a4a67040f09dfe107ea056f17
varunsdave <v2dave@uwaterloo.ca>
2014-09-16 18:05:24 -0400

    Create arrays_01.java

---
+++ arrays_01.java
@@ -0,0 +1,51 @@
+package com.varunsdave.ctci_01_01;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+/**
+ *  @author Varun Dave
+ *  @version 0.1
+ *  
+ *  This code provides solution to a problem that asks to determine 
+ *  whether a given string contains only unique chars. 
+ *  
+ *  The space requirement for this solution is n for the length 128
+ *  The code is tested for ASCII character set only.
+ *  
+ *  
+ */
+public class Solution {
+	
+	public static void main(String[] args) throws java.lang.Exception{
+		String pass_test = "sample";
+		String fail_test = "sample text"; //should fail at double t
+		
+		
+		
+		System.out.println("the result from pass_test is : " + containsUniqueChars(pass_test));
+		System.out.println("the result from fail_test is : "+ containsUniqueChars(fail_test));
+		
+	}
+	
+	private static boolean containsUniqueChars(String input_string){
+		if (input_string.length()>128){
+			return false;
+		}
+		
+		int [] char_occurences = new int [128];
+		
+		for (int i = 0; i< input_string.length(); i++){
+			int val = input_string.charAt(i);
+			char_occurences[val]++;
+			if (char_occurences[val] > 1){
+				
+				return false;
+			}
+		}
+		return true;
+	}
+	
+}
